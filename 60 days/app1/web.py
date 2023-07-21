480d482958b386b4a8e9de95566648a61f420f43
ivan.datko <ivandatko@gmail.com>
2023-07-21 16:52:59 +0200

    Implement "Add new toto items" feature

--- 60 days/app1/web.py
+++ 60 days/app1/web.py
@@ -3,6 +3,14 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    print(todos)
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+
 st.title('My Todo app')
 st.subheader('This is my todo app')
 st.write('This is a todo app that I made in 60 days of code')
@@ -12,4 +20,6 @@ for todo in todos:
     todo = todo.strip()
     st.checkbox(todo)
 
-st.text_input('Add a todo', placeholder='Add a todo here...')
+st.text_input(label="Add a todo", placeholder='Add a todo here...', on_change=add_todo, key='new_todo')
+
+st.session_state
